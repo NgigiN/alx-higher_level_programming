9ba015821fbba6511cb3db3f9fe048fb0593935e
NgigiN <ngigi.nyongo@gmail.com>
2023-03-27 21:58:54 +0300

    Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     final = 0
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
             final += 1
     except IndexError:
         print()

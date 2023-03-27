94cee48f6fe4bf0b3617d994251069f6cc6f3a76
NgigiN <ngigi.nyongo@gmail.com>
2023-03-27 21:46:11 +0300

    Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,7 @@ def safe_print_list(my_list=[], x=0):
 			print("{}".format(my_list[i]), end='')
 			final = final + 1
     	except IndexError:
-       		break
-    print("")
+       		print()
+            break
+    print()
     return (total)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        for item in my_list[:x]:
-            try:
-                print("{:d}".format(item), end='')
-            except (ValueError, TypeError):
-                pass
-    except (IndexError):
-        print("not in the list")
+    numby = 0
+
+    for item in range(x):
+        try:
+            print("{:d}".format(item), end='')
+            numby = numby + 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return numby

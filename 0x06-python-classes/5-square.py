230f064bcf9c10d8aae3e7a3fada86fdc7a6d5a8
NgigiN <ngigi.nyongo@gmail.com>
2023-03-29 00:15:20 +0300

    Classes and Objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,56 +1,59 @@
 #!/usr/bin/python3
-
-"""Defining a class square"""
+"""Defines a class Square"""
 
 
 class Square:
     """Represents a square
     Attributes:
-    __size (int): size of the square"""
-
-    def ___init__(self, size=0):
-        """initializes a square
+        __size (int): size of a side of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
         Args:
-        size(int):size of the square
-        position (int, int): the position of the square
+            size (int): size of a side of the square
+        Returns:
+            None
         """
+        self.size = size
 
-        self.size - size
+    def area(self):
+        """calculates the square's area
+        Returns:
+            The area of the square
+        """
+        return (self.__size) ** 2
 
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
 
-@property
-def size(self):
-    """set the current size of the square.
-    Returns:
-        size of the square"""
-    return (self.__size)
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
-
-@size.setter
-def size(self, value):
-    """
-    setter
-    Args:
-    value (int): size of the square
-    Returns:
-    None
-    """
-    if not isinstance(value, int):
-        raise TypeError("size must be an integer")
-    elif value < 0:
-        raise ValueError("size must be >= 0")
-    self.__size = value
-
-
-def area(self):
-    """Return area of square"""
-    return (self.__size ** self.__size)
-
-
-def my_print(self):
-    """Print the square with the # character."""
-    for i in range(0, self.__size):
-        [print("#", end="") for j in range(self.__size)]
-        print("")
-    if self.__size == 0:
-        print("")
+    def my_print(self):
+        """prints the square
+        Returns:
+            None
+        """
+        if self.__size == 0:
+            print()
+            return
+        for i in range(self.__size):
+            print("".join(["#" for j in range(self.__size)]))

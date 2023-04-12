5aaf475f499a6ab91da7ead3316530d45b0d62e4
NgigiN <ngigi.nyongo@gmail.com>
2023-04-12 23:57:18 +0300

    file input output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Add all arguments to python list and save them to a file"""
+"""
+Add all arguments to python list and save them to a file
+"""
 
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
@@ -12,5 +14,8 @@ if __name__ == "__main__":
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
         items = []
-    items.extend(sys.argv[1:])
+
+    for arg in argv[1:]:
+        items.append(arg)
+
     save_to_json_file(items, "add_item.json")

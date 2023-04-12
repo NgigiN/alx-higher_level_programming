5064e4be09f011fd8906367efe465b8c4a7f1ff4
NgigiN <ngigi.nyongo@gmail.com>
2023-04-12 23:44:45 +0300

    file input output

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A Student class"""
+
+
+class Student:
+    """Student blueprint"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+        def to_json(self):
+            """producds dict representation of the student"""
+            return self.__dict__

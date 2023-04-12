7c12209930efccfdab28ff35acd4eeeab7176ac2
NgigiN <ngigi.nyongo@gmail.com>
2023-04-13 00:02:36 +0300

    file input output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,17 +5,17 @@ Add all arguments to python list and save them to a file
 
 from sys import argv
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    for arg in argv[1:]:
-        items.append(arg)
+file = "add_item.json"
+try:
+    list_members = load_from_json_file(file)
+except FileNotFoundError:
+    list_members = []
 
-    save_to_json_file(items, "add_item.json")
+for arg in argv[1:]:
+    list_members.append(arg)
+
+save_to_json_file(list_members, file)

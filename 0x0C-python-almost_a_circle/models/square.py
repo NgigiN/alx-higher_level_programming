c3651baf33859b9cbcbd1346cabcdd4a6aa94174
NgigiN <ngigi.nyongo@gmail.com>
2023-04-21 09:09:13 +0300

    fixing setter for square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -29,6 +29,7 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
+        self.__size = value
         self.__width = value
         self.__height = value
 

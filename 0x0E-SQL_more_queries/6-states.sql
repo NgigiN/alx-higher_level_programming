8d52ae71077c7b7fad805df8e28fc52bd9b8fa41
NgigiN <ngigi.nyongo@gmail.com>
2023-05-19 00:06:14 +0300

    debugging

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -5,8 +5,4 @@
 -- script should not fail if database or table already exists
 
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
-	PRIMARY KEY('id'),
-	`id`	INT	NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(256) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -7,11 +7,7 @@
 -- script does not fail if database or table already exists
 
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`
-CREATE TABLE IF NOT EXISTS `cities` (
-	PRIMARY KEY(`id`)
-	`id` 	INT		NOT NULL AUTO_INCREMENT,
-	`state_id` INT		NOT NULL,
-	`name` 	VARCHAR(256)	NOT NULL
-	FOREIGN KEY(`state_id`)
-	REFERENCES (`hbtn_0d_usa`.`states`(`id`)
-);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));

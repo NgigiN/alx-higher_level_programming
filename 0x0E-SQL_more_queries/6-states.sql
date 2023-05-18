a8d5c088dde8062f30ad1fd12af87cc2ed76720d
NgigiN <ngigi.nyongo@gmail.com>
2023-05-18 23:55:08 +0300

    more sql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- script that creates the database: hbtn_0d_usa, table: states
+-- states description:
+--	id INT, unique, auto genereated, can't be null, primarykey
+--	name VARCHAR(256) can't be NULL
+-- script should not fail if database or table already exists
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+	PRIMARY KEY('id'),
+	`id`	INT	NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(256) NOT NULL
+);

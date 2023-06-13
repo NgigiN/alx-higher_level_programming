c9feb51a671584c55669bbb0bedd53f5f31ccf2b
NgigiN <ngigi.nyongo@gmail.com>
2023-06-13 06:26:40 +0300

    step 2

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function Biggy(args) {
+	if (args.length === 0 || args.length === 1) {
+		return 0;
+	}
+
+	args.sort((a, b) => a - b);
+	return args[args.length - 2];
+}
+
+if (process.argv.length > 1) {
+	console.log(`${Biggy(process.argv.slice(1))}`);
+} else {
+	console.log("0");
+}
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+exports.add = function (arg1, arg2) {
+  return arg1 + arg2;
+}

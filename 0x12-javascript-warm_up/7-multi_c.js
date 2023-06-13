fec24988e92a1dc778d61eb358184b40cf581e20
NgigiN <ngigi.nyongo@gmail.com>
2023-06-13 05:57:54 +0300

    semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@
 let i = 0;
 const myString = 'C is fun';
 
-if (Number(process.argv[2]) === parseFloat(process.argv[2])) {
+if (Number(process.argv[2]) === parseInt(process.argv[2])) {
   while (i < process.argv[2]) {
     console.log(myString);
     i++;
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-function add (arg1, arg2) {
-	if ((arg1 === undefined) || (arg2 === undefined)) {
-		console.log(`Nan`);
-	} else {
-		console.log(parseInt(arg1) + parseInt(arg2));
-	}
+function add(arg1, arg2) {
+  if ((arg1 === undefined) || (arg2 === undefined)) {
+    console.log(`Nan`);
+  } else {
+    console.log(parseInt(arg1) + parseInt(arg2));
+  }
 }
 
-if (process.argv.length >= 4){
-	add(process.argv[2], process.argv[3]);
+if (process.argv.length >= 4) {
+  add(process.argv[2], process.argv[3]);
 } else {
-	add(undefined, undefined);
+  add(undefined, undefined);
 }

983edc9c264880cd00a08fe32724c60f86025594
NgigiN <ngigi.nyongo@gmail.com>
2023-06-13 20:54:37 +0300

    fixing semistandard on 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
       return;
-    } else {
-      this.width = w;
-      this.height = h;
     }
+    this.width = w;
+    this.height = h;
   }
 };

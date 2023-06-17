56c8c10d932c5870e57aece2365e39e8c773fb41
NgigiN <ngigi.nyongo@gmail.com>
2023-06-17 19:02:13 +0300

    file 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 This script takes in an argument and
@@ -13,35 +12,20 @@ import MySQLdb
 import sys
 
 
-def list_states_arg(username, password, database, state_name):
-    """connection to MySQL server"""
-    connection = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+if __name__ == "__main__":
+    """
+    Access to the database
+    """
+connection = MySQLdb.connect(host="localhost", port=3306,
+                             user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
-    cursor = connection.cursor()
-    """connect  to the MySQL server safely"""
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY \
-                        states.id ASC".format(sys.argv[4]))
+cursor = connection.cursor()
 
-    results = cursor.fetchall()
+cursor.execute(
+    "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': sys.argv[4]})
 
-    for row in results:
-        print(row)
+results = cursor.fetchall()
 
-    cursor.close()
-    connection.close()
-
-
-if __name__ == '__main__':
-    """Gets arguents from the command line"""
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    list_states_arg(username, password, database, state_name)
+for row in results:
+    print(row)

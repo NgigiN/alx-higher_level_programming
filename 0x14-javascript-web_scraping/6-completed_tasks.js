8103a67f433e0b31b7add5bb0cf79e3f0e6877ce
NgigiN <ngigi.nyongo@gmail.com>
2023-07-26 22:32:39 +0300

    correcting 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,26 @@
 #!/usr/bin/node
 // Script that computes the number of tasks completed by user id
 
-const request = require('request');
 const url = process.argv[2];
+const request = require('request');
 
 request(url, function (err, response, body) {
   if (err) {
-    return console.log(err);
-  }
-  const todos = JSON.parse(body);
-
-  const completed = {};
-  for (const todo of todos) {
-    if (todo.completed === true) {
-      if (completed[todo.userId] === undefined) {
-        completed[todo.userId] = 1;
-      } else {
-        completed[todo.userId] += 1;
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const dict = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      if (tasks[i].completed) {
+        if (dict[tasks[i].userId] === undefined) {
+          dict[tasks[i].userId] = 1;
+        } else {
+          dict[tasks[i].userId]++;
+        }
       }
     }
+    console.log(dict);
+  } else {
+    console.log('Error code: ' + response.statusCode);
   }
-  console.log(completed);
-}
-);
+});

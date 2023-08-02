8d0d4b555ad9d2500422fdc44e62d56272801319
NgigiN <ngigi.nyongo@gmail.com>
2023-08-02 22:16:56 +0300

    adding 1

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Script that updates the text color of the <header> element to red (#FF0000)
+// can't use document.querySelector to select the HTML tag
+// Must use JQuery API
+
+$('header').css('color', '#FF0000')
